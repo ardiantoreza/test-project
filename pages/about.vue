ce77f6dff959cb8650f322b42a985ea77d27ba15
Reza Ardianto <r3z496@gmail.com>
2023-09-22 00:06:01 +0700

    update

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nihil minus autem quisquam atque sit aliquid
+            laborum. Voluptas adipisci recusandae blanditiis maiores error facere quisquam dolore esse. Aliquid,
+            voluptatibus voluptatem!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+h2 {
+
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+
+    margin: 20px 0;
+}
+</style>
